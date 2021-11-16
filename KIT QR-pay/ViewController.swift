491fa1fe3c56f655c04532ea7c10ac6e93373732
obaibuz <abaybuz@ukr.net>
2021-11-16 08:38:47 +0200

    Initial Commit

---
+++ KIT QR-pay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KIT QR-pay
+//
+//  Created by Baibuz Oleksandr on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
