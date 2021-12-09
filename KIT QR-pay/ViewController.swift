a663b0829d2d3448769c3d34c470b8d942109b6a
obaibuz <abaybuz@ukr.net>
2021-12-09 15:52:26 +0200

    First

--- KIT QR-pay/ViewController.swift
+++ KIT QR-pay/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var qrImageOutlet: UIImageView!
+    
+    var structQR: QRStructDecoder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tappedQRimage))
+        qrImageOutlet.addGestureRecognizer(tap)
+        qrImageOutlet.isUserInteractionEnabled = true
+    }
+    
+    @objc func tappedQRimage(_ sender: Any) {
+        performSegue(withIdentifier: "openScannerSegue", sender: nil)
     }
 
+    //--------------
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "openScannerSegue"  {
+            let contoller = (segue.destination as! UINavigationController).topViewController as! ScannerViewController
+            contoller.didReadQRStruct  =  {  (ReadQRStruct) in
+                self.structQR = ReadQRStruct
+                self.performSegue(withIdentifier: "openPaySegue", sender: nil)
+            }
+        }
+        if segue.identifier == "openPaySegue"  {
+            let contoller = (segue.destination as! UINavigationController).topViewController as! applePayViewController
+            contoller.structQR = self.structQR
+        }
+    }
 
 }
 
